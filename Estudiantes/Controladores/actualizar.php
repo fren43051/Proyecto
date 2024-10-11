432ed68944ea4d47a45741b1f56f382ed26e8628
nelson reyes <32681314+fren43051@users.noreply.github.com>
2024-10-10 23:36:29 -0600

    Agregando codigo video funcionalidades 2

--- Estudiantes/Controladores/actualizar.php
+++ Estudiantes/Controladores/actualizar.php
@@ -1,6 +1,16 @@
-<?php
+if($_POST){
+$ModeloEstudiantes = new Estudiantes();
 
-class actualizar
-{
+$Nombre = $_POST['Nombre'];
+$Apellido = $_POST['Apellido'];
+$Documento = $_POST['Documento'];
+$Correo = $_POST['Correo'];
+$Materia = $_POST['Materia'];
+$Docente = $_POST['Docente'];
+$Promedio = $_POST['Promedio'];
+$Fecha = date('Y-m-d');
+$ModeloEstudiantes->add($Nombre, $Apellido, $Documento, $Correo, $Materia, $Docente, $Promedio, $Fecha);
 
+}else{
+header("Location: ../../Index.php");
 }
---
+++ fechas.php
@@ -0,0 +1,6 @@
+<?php
+
+$fecha = date('Y-m-d');
+
+
+?>
